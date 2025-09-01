de178f323e00db6c8c5fd806cdee6b89f7c9308f
aom081 <matemsom@gmail.com>
2025-09-01 15:44:15 +0700

    fix: add vercel.json configuration for routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import router from "./Routes/router.jsx"; // 
+import router from "./Routes/router.jsx";
 import { RouterProvider } from "react-router-dom"; 
 import NavBar from "./components/NavBar";
 import { AuthProvider } from "./context/AuthContext";
