a27901d5b0beda0616b73835d96c593907a2c114
aom081 <matemsom@gmail.com>
2025-09-01 15:41:46 +0700

    fix: update RouterProvider import to use 'react-router-dom' for consistency

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import router from "./Routes/router.jsx";
-import { RouterProvider } from "react-router";
+import router from "./Routes/router.jsx"; // 
+import { RouterProvider } from "react-router-dom"; 
 import NavBar from "./components/NavBar";
 import { AuthProvider } from "./context/AuthContext";
 
