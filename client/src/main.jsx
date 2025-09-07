06dcef47e24989e06c2d2b9ed5b278f46506a0cd
aom081 <matemsom@gmail.com>
2025-09-07 23:51:58 +0700

    fix: update import path for router in main.jsx to include file extension

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import router from "./routes/Router";
+import router from "./routes/Router.jsx";
 import { RouterProvider } from "react-router";
 import NavBar from "./components/NavBar";
 import { AuthProvider } from "./context/AuthContext";
